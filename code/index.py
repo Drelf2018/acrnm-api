7c62244b5cfbd9b33c926a8dcf291cb933b14709
Drelf2018 <drelf2018@outlook.com>
2024-03-10 12:34:16 +0800

    feat: 启动

--- code/index.py
+++ code/index.py
@@ -1,38 +1,58 @@
+from dataclasses import dataclass, field
+from typing import List
+
+import cloudscraper
 import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
+from lxml import etree
 
 app = FastAPI()
+scraper = cloudscraper.create_scraper()
+
+@dataclass
+class Variant:
+    color: List[str]
+    size: List[str]
+
+@dataclass
+class Product:
+    name: str
+    price: str
+    variants: List[Variant] = field(default_factory=list)
 
 
-@app.get('/')
-async def hello():
-    return HTMLResponse('''<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Serverless Devs - Powered By Serverless Devs</title>
-    <link href="https://example-static.oss-cn-beijing.aliyuncs.com/web-framework/style.css" rel="stylesheet" type="text/css"/>
-</head>
-<body>
-<div class="website">
-    <div class="ri-t">
-        <h1>Devsapp</h1>
-        <h2>这是一个 FastAPI 项目</h2>
-        <span>自豪的通过Serverless Devs进行部署</span>
-        <br/>
-        <p>您也可以快速体验： <br/>
-            • 下载Serverless Devs工具：npm install @serverless-devs/s<br/>
-            • 初始化项目：s init start-fastapi<br/>
+def get_page_root() -> etree._Element:
+    resp = scraper.get("https://acrnm.com/index?sort=default&filter=txt").text
+    return etree.HTML(resp)
 
-            • 项目部署：s deploy<br/>
-            <br/>
-            Serverless Devs 钉钉交流群：33947367
-        </p>
-    </div>
-</div>
-</body>
-</html>
-''')
+
+def parse_products(root: etree._Element) -> List[Product]:
+    products = []
+    table: List[etree._Element] = root.cssselect(".m-product-table__row")
+    for tr in table:
+        price = tr.xpath("./td[4]/span/text()")
+        if len(price) == 0:
+            continue
+        product = Product(tr.xpath("./td[1]/a/span/text()")[0], price[0])
+        variants: List[etree._Element] = tr.xpath("./td[3]/div/span")
+        for var in variants:
+            product.variants.append(
+                Variant(var.xpath("./div/span/text()"), var.xpath("./span/text()")))
+        products.append(product)
+    return products
+
+
+@app.get("/")
+def _():
+    root = get_page_root()
+    products = parse_products(root)
+    return products
+
+
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse("favicon.ico")
 
 
 if __name__ == "__main__":
