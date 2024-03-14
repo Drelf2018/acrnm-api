a05d4873f45521c05d5a51db5982ef5c9d84a7ac
Drelf2018 <drelf2018@outlook.com>
2024-03-14 14:36:49 +0800

    feat: 新增图片获取

--- code/index.py
+++ code/index.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from typing import List
 
 import cloudscraper
+import httpx
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
@@ -12,12 +13,13 @@ scraper = cloudscraper.create_scraper()
 
 @dataclass
 class Variant:
-    color: List[str]
-    size: List[str]
+    color: str
+    size: str
 
 @dataclass
 class Product:
     name: str
+    href: str
     price: str
     variants: List[Variant] = field(default_factory=list)
 
@@ -34,17 +36,18 @@ def parse_products(root: etree._Element) -> List[Product]:
         price = tr.xpath("./td[4]/span/text()")
         if len(price) == 0:
             continue
-        product = Product(tr.xpath("./td[1]/a/span/text()")[0], price[0])
+        product = Product(tr.xpath("./td[1]/a/span/text()")[0], tr.xpath("./td[1]/a/@href")[0], price[0])
         variants: List[etree._Element] = tr.xpath("./td[3]/div/span")
         for var in variants:
-            product.variants.append(
-                Variant(var.xpath("./div/span/text()"), var.xpath("./span/text()")))
+            color = ", ".join(var.xpath("./div/span/text()"))
+            size = ", ".join(var.xpath("./span/text()"))
+            product.variants.append(Variant(color, size))
         products.append(product)
     return products
 
 
 @app.get("/")
-def _():
+def index():
     root = get_page_root()
     products = parse_products(root)
     return products
@@ -53,6 +56,14 @@ def _():
 @app.get("/favicon.ico")
 def favicon():
     return FileResponse("favicon.ico")
+
+
+@app.get("/image/{name}")
+def get_view(name: str):
+    resp = scraper.get(f"https://acrnm.com/{name}").text
+    root: etree._Element = etree.HTML(resp)
+    src = root.xpath("/html/body/div[1]/main/turbo-frame/div/div[2]/div[1]/img/@src")
+    return src[0]
 
 
 if __name__ == "__main__":
